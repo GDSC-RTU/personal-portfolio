6ba34c19033797897f85e94f54ba01db9b7a89f6
ishubham21 <sg2199203@gmail.com>
2021-09-02 22:53:02 +0530

    fix cursor

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ let cursor = document.querySelector(".cursor");
 let cursor2 = document.querySelector(".cursor2");
 let tagline = document.querySelector('.tagline')
 
-
 document.addEventListener('mousemove', function(e){
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
   cursor2.style.left = e.clientX + 'px';
   cursor2.style.top = e.clientY + 'px';
 });
 
+tagline.addEventListener('mouseover', () => {
+  cursor.classList.add('taglineHover')
+  cursor2.style.display = 'none'
+})
 
-
+tagline.addEventListener('mouseleave', () => {
+  cursor.classList.remove('taglineHover')
+  cursor2.style.display = 'block'
+})
 
 
