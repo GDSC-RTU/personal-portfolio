8a773e4139c51afb8af1c69505af63fc2c1edd5f
ishubham21 <sg2199203@gmail.com>
2021-09-02 19:06:23 +0530

    fixing cursor

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-var cursor = document.querySelector(".cursor");
-var cursor2 = document.querySelector(".cursor2");
-document.addEventListener("mousemove",function(e){
-    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+let cursor = document.querySelector(".cursor");
+let cursor2 = document.querySelector(".cursor2");
+let tagline = document.querySelector('.tagline')
+
+
+document.addEventListener('mousemove', function(e){
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+  cursor2.style.left = e.clientX + 'px';
+  cursor2.style.top = e.clientY + 'px';
 });
+
+
+
+
+
