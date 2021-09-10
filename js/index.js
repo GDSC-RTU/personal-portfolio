a2e847f9c4384b91d08ca5725288b95a376af2a0
khush2706 <kc220313@gmail.com>
2021-09-10 12:24:54 +0530

    added back to home button in 404.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("random msg");
+// const links = document.querySelectorAll('.offcanvas-link')
+// const close = document.querySelector('.btn-close')
+// for(let i=0; i < links.length; i++){
+//   links[i].addEventListener('click',() => {close.click()}
+// }
+
+document.addEventListener("DOMContentLoaded", function(){
+   var links = document.querySelectorAll(".offcanvas-link");
+   for(var x=0; x<links.length; x++){
+      links[x].onclick = function(){
+         document.querySelector("button.btn-close").click();
+      }
+   }
+});
